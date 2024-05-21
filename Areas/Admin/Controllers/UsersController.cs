e4c90484d63e62aea3f97f4735537a0c2beeb4fe
Kovalchuk-Nata <natanata2810@gmail.com>
2024-05-21 17:49:08 +0300

    Edit User in Admin Panel

--- Areas/Admin/Controllers/UsersController.cs
+++ Areas/Admin/Controllers/UsersController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SchoolFinderWeb.Areas.Admin.ViewModel;
+using SchoolFinderWeb.Areas.Identity.Pages.Account;
 using SchoolFinderWeb.Data;
 using SchoolFinderWeb.Models;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 
-namespace SF.Areas.Admin.Controllers
+namespace SchoolFinderWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = "Admin")]
@@ -14,11 +20,36 @@ namespace SF.Areas.Admin.Controllers
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext schoolDB;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private readonly IUserStore<User> _userStore;
+        private readonly IUserEmailStore<User> _userEmailStore;
 
-        public UsersController(ApplicationDbContext schoolDB)
+        public UsersController(ApplicationDbContext schoolDB,
+            UserManager<User> userManager,
+            IUserStore<User> userStore,
+            SignInManager<User> signInManager,
+            ILogger<RegisterModel> logger,
+            IUserEmailStore<User> emailStore,
+            IEmailSender emailSender)
         {
             this.schoolDB = schoolDB;
+            _userManager = userManager;
+            _userStore = userStore;
+            _userEmailStore = emailStore;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
         }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+
 
         public IActionResult Index()
         {
@@ -29,64 +60,142 @@ namespace SF.Areas.Admin.Controllers
         //GET
         public IActionResult Create()
         {
-            return View();
+
+            return View(new InputModel());
         }
 
         //POST
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult Create(User obj)
+        public async Task<IActionResult> Create(InputModel obj)
         {
+            //returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                schoolDB.Users.Add(obj);
-                schoolDB.SaveChanges();
-                return RedirectToAction("Index");
+                var user = CreateUser();
+
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                user.UserType = Input.UserType;
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userEmailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    // Додати користувача до ролі
+                    if (user.UserType == UserTypes.Parent)
+                    {
+                        _userManager.AddToRoleAsync(user, "Parent").Wait();
+                    }
+                    else if (user.UserType == UserTypes.Organizator)
+                    {
+                        _userManager.AddToRoleAsync(user, "Organizator").Wait();
+                    }
+                    else if (user.UserType == UserTypes.Admin)
+                    {
+                        _userManager.AddToRoleAsync(user, "Admin").Wait();
+                    }
+
+
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    user.EmailConfirmed = true;
+                    await _userManager.UpdateAsync(user);
+
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index"); //LocalRedirect(returnUrl);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
+            
             return View(obj);
         }
 
-        /*
+        ////POST
+        //[HttpPost]
+        //[AutoValidateAntiforgeryToken]
+        //public IActionResult Create(User obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        schoolDB.Users.Add(obj);
+        //        schoolDB.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(obj);
+        //}
+
+        
         //TODO сделать правильную работу в редактировании юзера в поле пароль
         //GET
-        public IActionResult Edit(int? id)
+        public IActionResult Edit(string? id)
         {
-            if (id == null || id == 0)
+            if (id == null)
             {
                 return NotFound();
             }
-            var userId = schoolDB.Users.FirstOrDefault(c => c.UserID == id);
+
+            var userId = schoolDB.Users.FirstOrDefault(c => c.Id == id);
 
             if (userId == null)
             {
                 return NotFound();
             }
 
-            return View(userId);
+            var userView = new EditUserViewModel
+            {
+                Id = userId.Id,
+                UserType = userId.UserType,
+                FirstName = userId.FirstName,
+                LastName = userId.LastName
+            };
+            
+            
+
+            return View(userView);
         }
 
         //POST
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult Edit(User obj)
+        public IActionResult Edit(EditUserViewModel obj)
         {
-            if (ModelState.IsValid)
-            {
-                schoolDB.Users.Update(obj);
+
+            //if (ModelState.IsValid)
+            //{
+                var user = schoolDB.Users.FirstOrDefault(c => c.Id == obj.Id);
+
+                if(user == null)
+                {
+                    return NotFound();
+                }
+
+                user.FirstName = obj.FirstName;
+                user.LastName = obj.LastName;
+                user.UserType = obj.UserType;
+                schoolDB.Users.Update(user);
                 schoolDB.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(obj);
+            
+            return RedirectToAction("Index");
+            //}
+            //return View(obj);
+
         }
 
         //GET
-        public IActionResult Delete(int? id)
+        public IActionResult Delete(string? id)
         {
-            if (id == null || id == 0)
+            if (id == null)
             {
                 return NotFound();
             }
-            var user = schoolDB.Users.FirstOrDefault(c => c.UserID == id);
+            var user = schoolDB.Users.FirstOrDefault(c => c.Id == id);
 
             return View(user);
         }
@@ -95,9 +204,10 @@ namespace SF.Areas.Admin.Controllers
         //POST
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult Delete(int id)
+        public IActionResult Delete(User user)
         {
-            var obj = schoolDB.Users.Find(id);
+            var userId = user.Id;
+            var obj = schoolDB.Users.Find(userId);
             if (obj == null)
             {
                 return NotFound();
@@ -107,6 +217,49 @@ namespace SF.Areas.Admin.Controllers
             schoolDB.SaveChanges();
             return RedirectToAction("Index");
         }
-        */
+        
+        private User CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<User>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(User)}'. " +
+                    $"Ensure that '{nameof(User)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+    }
+    public class InputModel
+    {
+        [Required]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Display(Name = "UserType")]
+        public UserTypes UserType { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
+        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        //public string ConfirmPassword { get; set; }
     }
 }
---
+++ Areas/Admin/ViewModel/EditUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SchoolFinderWeb.Models;
+
+namespace SchoolFinderWeb.Areas.Admin.ViewModel
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        public UserTypes UserType { get; set;}
+
+    }
+}
